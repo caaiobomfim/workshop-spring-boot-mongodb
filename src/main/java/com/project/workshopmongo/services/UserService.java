c8be5d75a79d8d4078df39f49d05dbaaf4195647
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-19 22:05:58 -0300

    [PROJECT] - Deleção de usuário com DELETE

--- src/main/java/com/project/workshopmongo/services/UserService.java
+++ src/main/java/com/project/workshopmongo/services/UserService.java
@@ -28,7 +28,13 @@ public class UserService {
         return userRepository.insert(user);
     }
 
+    public void delete(String id){
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
     public User fromDTO(UserDTO userDTO){
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
+
 }
